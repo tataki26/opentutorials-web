3fc3777d01d09be94c6e066b80610f02ebf6cced
tataki26 <tataki26@gmail.com>
2022-10-05 17:24:03 +0900

    Add jQuery in ex3.html and update colors.js
    - to learn how to use library and to apply jQuery

--- js/colors.js
+++ js/colors.js
@@ -1,15 +1,17 @@
 // same function name, different object
 var Body = {
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
 }
 var Links = {
     setColor: function (color) {
-        let alist = document.querySelectorAll('a');
-        for(let i = 0; i < alist.length; i++){
-            alist[i].style.color = color;
-        }
+        // let alist = document.querySelectorAll('a');
+        // for(let i = 0; i < alist.length; i++){
+        //     alist[i].style.color = color;
+        // }
+        $('a').css('color', color);
     },
 }
 // 기존에 사용하던 this를 그대로 두면 전역 변수로서 window를 받게 된다
