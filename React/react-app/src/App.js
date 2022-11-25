ea86f4273e4597cb78c4b2ea44057e595f0689b4
tataki26 <tataki26@gmail.com>
2022-11-25 16:54:27 +0900

    Review status

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -50,7 +50,8 @@ function Nav(props) {
         // 아래에서 정의된 onChangeMode ---> id를 파라미터로 받는다
         // target: 이벤트를 유발시킨 태그(a) --> 이벤트 함수 안에서 a 태그의 id 속성을 가져온다 
         // a 태그 옆 id는 파라미터로 사용될 변수명을 의미한다
-        // 태그의 속성으로 넘길 때 문자열로 바뀌므로 다시 숫자로 변경
+        // **태그의 속성으로 넘길 때 문자열로 바뀌므로 다시 숫자로 변경**
+        // ---> 그래야 state id와 topics의 id를 비교할 수 있다
         props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
       </li>)
@@ -97,17 +98,25 @@ function Create(props) {
   </article>
 }
 
+/*
+컴포넌트의 입력은 prop, 출력은 처리된 데이터로 만들어지는 UI
+prop은 컴포넌트를 사용하는 외부자를 위한 data
+state는 컴포넌트를 만드는 내부자를 위한 data
+*/
+
 function App() {
   // 상태 생성
   // 상태를 리턴하는 함수 useState
   // 상태의 0번째 원소: 상태의 값, 인자('WELCOME')
   // 상태의 1번째 원소: 상태의 값을 변경할 때 사용하는 함수
-  // const _mode = useState('WELCOME');
+  // const _mode = useState('WELCOME'); // 초기값 - 'WELCOME'
   // const mode = _mode[0];
-  // const setMode = _mode[1];
-  // [같은 코드]
+  // const setMode = _mode[1]; ---> setMode로 mode의 값을 바꿀 수 있다
+  // [같은 코드]: 지역 변수(mode)를 상태로 업그레이드
   const [mode, setMode] = useState('WELCOME');
   // 새로운 id 값 지정
+  // READ를 그대로 출력하지 않고 선택한 topic이 동적으로 나오도록 변경
+  // 선택한 topic의 상태를 관리
   const [id, setId] = useState(null);
   // 3까지는 이미 완성되어 있으니 4부터 시작
   const [nextId, setNextId] = useState(4);
@@ -123,19 +132,23 @@ function App() {
     {id: 2, title: 'css', body: 'css is ...'},
     {id: 3, title: 'js', body: 'js is ...'},
   ]);
-  let content = null;
-  // mode에 따라 내용이 달라짐
+  // const mode = 'WELCOME';
+  let content = null; // ---> 본문
+  // mode에 따라 본문(content)이 달라짐
   if (mode === 'WELCOME') {
-    content = <Article title="Welcome" body="Hello, WEB"></Article>
+    content = <Article title="Welcome" body="Hello, WEB"></Article> // welcome page
   } else if (mode === 'READ') {
+    // content = <Article title="Read" body="Hello, Read"></Article> // read page
+    // title과 body를 동적으로 변경
     let title, body = null;
-    // id가 같은 것 찾기
+    // id state와 일치하는 topic 찾기
     for(let i=0; i<topics.length; i++){
-      if(topics[i].id === id) {
+      if(topics[i].id === id) { // topics의 id와 id state가 일치하면,
         title = topics[i].title;
         body = topics[i].body;
       }
     }
+    // 순회한 결과를 태그에 추가
     content = <Article title={title} body={body}></Article>
   } else if (mode === 'CREATE') {
     content = <Create onCreate={(_title, _body) => {
@@ -157,16 +170,22 @@ function App() {
           Header 컴포넌트 안에서 링크를 클릭하면 인자로 넘긴 함수를 호출한다 */}
       <Header title="WEB" onChangeMode={()=>{
         // 변수의 값을 바꿔도 상태는 변하지 않는다
-        // mode = 'WELCOME'
-        // 상태 함수를 통해 값을 바꿔야 한다
+        // mode = 'WELCOME';
+        // 상태 함수를 통해 값을 바꿔야 한다(setter)
         // App 컴포넌트를 다시 실행해서 값이 갱신되는 원리이다
         setMode('WELCOME');
       }}></Header>
       {/* 중괄호 안 topics는 위에서 정의한 topics 변수 */}
       <Nav topics={topics} onChangeMode={(_id)=>{
+        // mode = 'READ'; --> App 함수가 다시 실행되지 않아 UI에 변화가 생기지 않는다
+        // useState가 mode의 값을 'READ'로 세팅
         setMode('READ');
-        setId(_id);
+        // Nav 컴포넌트의 링크를 클릭할 때, id도 바뀜
+        setId(_id); // _id는 Nav 내부에서 정의된다
       }}></Nav>
+      {/* <Article title="Welcome" body="Hello, WEB"></Article>을
+          content 변수로 대체함
+          HTML 내부이므로 중괄호로 변수 사용 */}
       {content}
       <a href="/create" onClick={event=>{
         event.preventDefault();
